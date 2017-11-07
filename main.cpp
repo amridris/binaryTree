47c8d4a9f0aa18f76b122f097c73a5a3a2815692
Amer00100 <amridris@gmail.com>
2017-11-07 02:50:53 +0000

    main.cpp edited online with Bitbucket

--- main.cpp
+++ main.cpp
@@ -1,76 +1,47 @@
-#include "../inc/tree.h"
-namespace lab7 {
-    void clear(node *to_clear);
 
-    void node_print_gtl(node *to_print);
+#include<iostream>
+#include "tree.h"
+#include "node.h"
 
-    // Construct an empty tree
-    tree::tree() {
-        root = nullptr;
+using namespace std;
+
+int main(){
+
+    int check_number;
+    tree s;
+
+    s.insert(50);
+    s.insert(57);
+    s.insert(33);
+    s.insert(29);
+    s.insert(29);
+    s.insert(39);
+    s.insert(39);
+    s.insert(39);
+    s.insert(39);
+    s.insert(45);
+    s.print();
+    cout<<"The size of the tree is "<<s.size()<<endl;
+    cout<<"The frequency of 39 is "<<s.get_frequency(39)<<endl;
+    cout<<"The depth of the tree is: "<<s.depth()<<endl;
+    cout<<"The level of key 45 is: "<<s.level(45)<<endl;
+    cout<<"The level of key 39 is: "<<s.level(39)<<endl;
+    cout<<"The level of key 50 is: "<<s.level(50)<<endl;
+
+    cout<<"The value inserted is: "<<s.in_tree(29)<<endl;
+    cout<<"The value inserted is: "<<s.in_tree(15)<<endl;
+    cout<<"please enter a number to see if it is in the tree"<<endl;
+    cin>>check_number;
+    s.in_tree(check_number);
+    if(check_number)
+    {
+        cout<<"Number found!"<<endl;
+    }
+    else
+    {
+        cout<<"Number not found!"<<endl;
     }
 
-    // Deconstruct tree
-    tree::~tree() {
-        clear(root);
-    }
 
-    // Insert
-    void tree::insert(int value) {
-
-    }
-
-    // Remove key
-    bool tree::remove(int key) {
-
-    }
-
-    // What level is key on?
-    int tree::level(int key) {
-
-    }
-
-    // Print the path to the key, starting with root
-    void tree::path_to(int key) {
-
-    }
-
-    // Number of items in the tree
-    unsigned tree::size() {
-
-    }
-
-    // Calculate the depth of the tree, longest string of connections
-    unsigned tree::depth() {
-
-    }
-
-    // Determine whether the given key is in the tree
-    bool tree::in_tree(int key) {
-
-    }
-
-    // Return the number of times that value is in the tree
-    int tree::get_frequency(int key) {
-
-    }
-
-    // Print the tree least to greatest, Include duplicates
-    void tree::print() {
-
-    }
-
-    void tree::print_gtl() {
-        //WILL BE PROVIDED FOR YOU
-    }
-
-    void node_print_gtl(node *to_print) {
-        //WILL BE PROVIDED FOR YOU
-    }
-
-    void clear(node *to_clear) {
-        if (to_clear == nullptr) return;
-        if (to_clear->left != nullptr) clear(to_clear->left);
-        if (to_clear->right != nullptr) clear(to_clear->right);
-        delete to_clear;
-    }
+    return 0;
 }
